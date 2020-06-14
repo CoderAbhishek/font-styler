fa565f4fb98568618f10f0860725fed973b755af
Abhishek Vinod <abhishekvinod48@gmail.com>
2020-06-14 18:23:03 +0530

    structured

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserDataComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule
