37ec3d23fe6d410e3213db747ad1b08e401e7470
Abhishek Vinod <abhishekvinod48@gmail.com>
2020-06-15 22:46:55 +0530

    started

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserDataComponent } from './user-data/user-data.component';
 import { DisplayComponent } from './display/display.component';
+import { AnalysisComponent } from './analysis/analysis.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserDataComponent,
-    DisplayComponent
+    DisplayComponent,
+    AnalysisComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
